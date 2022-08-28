be2e5d65618ea67c53003a4fbb4829fab51f4778
galaxy-comet <oleksandrshkabura811@gmail.com>
2022-08-28 17:07:50 +0800

    feat: reusable spinner component implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SpinnerComponent],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule, LayoutModule],
   providers: [],
   bootstrap: [AppComponent],
